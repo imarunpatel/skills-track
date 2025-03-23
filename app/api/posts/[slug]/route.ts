ad646f44865846725c68757587eb8789afad3e97
Arun Patel <arunpatel209@gmail.com>
2025-03-23 15:38:32 +0530

    env fix

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabase";
+import { supabaseAdmin } from "@/lib/supabase";
 import { NextRequest, NextResponse } from "next/server";
 
 
@@ -6,10 +6,10 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest, { params } : { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
 
-    const { data: post, error } = await supabase
+    const { data: post, error } = await supabaseAdmin
         .from('posts')
-        .select('*, author:users(id, name, email, image)').eq('slug', slug).single();
-
+        .select('*, author:users(id, name, image)').eq('slug', slug).single();
+        
     if(error || !post) {
         return NextResponse.json({ error: "Post not found" }, { status: 404 });
     }
