c38e0b48c2deb25b4e0f882fb728435b38de579f
Arun Patel <arunpatel209@gmail.com>
2025-03-23 16:14:24 +0530

    env update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseClient = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // Use service role key for write access
 );
 
 export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! // Use service role key for write access
 );
 
